74b79d80b04cfe255ed7d02cf0cc147975298954
graphinfinit <72710949+graphinfinit@users.noreply.github.com>
2021-05-26 17:36:47 +0300

    Update settings.py

--- modules/settings.py
+++ modules/settings.py
@@ -10,7 +10,7 @@ settings = sqly.select_settings()
 TOKEN = settings[0]['token']
 IMAGE_LIMIT = settings[0]['count_foto']
 
-#TOKEN = "OuDNh4liWBnpWRogRUWX4OfkSLCI"
+#TOKEN = "////"
 
 # количество фото прикрепляемых к объявлению
 #IMAGE_LIMIT = 5
